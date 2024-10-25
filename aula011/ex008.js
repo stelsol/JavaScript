f000d5b382a9d8220783c0b0ed2105e2d0c01216
stelsol <132797421+stelsol@users.noreply.github.com>
2024-10-25 14:15:12 -0300

    Criei um estilo basico para o ex

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,5 +1,6 @@
-var vel= 65
+var vel= 15
 console.log(`Sua velocidade é de ${vel}km/h`)
 if(vel > 60){
     console.log(`você ultrapassou a velocidade permitida!`)
 }
+console.log(`Dirija sempre usando cinto de segurança`)

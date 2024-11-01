e189b566b97d8fb2fc7f4fba402ed9af3966f388
stelsol <132797421+stelsol@users.noreply.github.com>
2024-11-01 10:54:21 -0300

    Condições aninhadas

--- aula0012/ex11.js
+++ aula0012/ex11.js
@@ -1,6 +1,9 @@
-var id = 22
-if(id < 18){
+var id = 12 
+console.log(`Você tem ${id} anos seu voto é: `)
+if(id < 16){
     console.log('Não vota')
-}else{
-    console.log('Vota')
+} else if( id < 18 || id > 65){
+     console.log('Opcional!')
+} else {
+    console.log('Obrigatório!!')
 }
---
+++ aula0012/ex12.js
@@ -0,0 +1,22 @@
+var agora = new Date() // Aprendemos a cplocar o horario do sistema atual com está função nova!
+var hr = agora.getHours()
+console.log(`Agora são ${hr} hrs`)
+    if(hr < 5){
+        console.log('Boa Madrugada!')
+    } else if(hr < 12) {
+        console.log('Bom Dia!')
+    }else if(hr < 19){
+        console.log('Boa tarde!')
+    } else{
+        console.log('Boa noite')
+    }
+
+  /*  var hr = 12
+console.log(`Agora são ${hr} hrs`)
+    if(hr >= 1 && hr < 12){
+        console.log('Bom Dia !')
+    } else if(hr >= 12 && hr < 19) { 
+        console.log('Boa Tarde!')
+    }else {
+        console.log('Boa noite !')
+    }*/

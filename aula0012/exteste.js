24ee32046ddb4a30dd65d67ef6ce7d8848614b89
stelsol <132797421+stelsol@users.noreply.github.com>
2024-11-04 10:02:37 -0300

    Aprendendo

---
+++ aula0012/exteste.js
@@ -0,0 +1,10 @@
+var nota = 4
+    if(nota >= 9 && nota <= 10){
+        console.log('Aprovado com exelência')
+    } else if(nota >= 7 && nota <=8){
+        console.log('Aprovado')
+    } else if(nota >= 5 && nota <= 7){
+        console.log('Na média')
+    }else {
+        console.log('Recuperação')
+    }

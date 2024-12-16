b7ae2c2ed2f23a2db2676fb72de07cb5f9a49a9a
stelsol <132797421+stelsol@users.noreply.github.com>
2024-12-16 20:45:38 -0300

    Repetição inicio e final

---
+++ aula0013/ambiente.js
@@ -0,0 +1,28 @@
+/* Estrutura de repetição com teste lógio no Final */
+/*
+var c = 1 
+do{
+    console.log(`atividade${c}`)
+    c++
+}while(c <= 7)
+
+
+
+
+/* Estrutura de repetição com teste lógio no inicio */
+/*
+var c = 1
+while(c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+/*
+Pior maneira!! você escreve cada uma !
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+*/
---
+++ aula0013/testes.js
@@ -0,0 +1,15 @@
+var pz = 1
+do{
+    console.log(`comi a fatia ${pz}`)
+    pz++
+} while( pz <= 8)
+    console.log('Acabou a pizza!')
+
+
+
+/*var pz = 1
+while (pz <= 8 ){
+    console.log(`Comi Pedaço ${pz}`)
+    pz++
+}
+console.log('Pizza acabou!!')*/
